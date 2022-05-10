c18de36286c0f8b76ef29c46be913dc147166236
patricksoftware13 <53525361+patricksoftware13@users.noreply.github.com>
2022-05-09 21:33:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
+    return "Quem sabe faz ao VIVO!"
 
 if __name__ == '__main__':
     app.run()
